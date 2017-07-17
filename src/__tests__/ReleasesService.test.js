2502cd9c2139f4dd5170631f89665e5da925ab57
Neil Kempin <neilkempin@gmail.com>
2017-07-17 01:46:49 -0400

    fix Releases Service tests
    - releaseService.makeRequest monkey patching function was accessing mock
      json data incorrectly, now this function supplies the correct data
    - err.stack in catch blocks removed

--- src/__tests__/ReleasesService.test.js
+++ src/__tests__/ReleasesService.test.js
@@ -16,7 +16,7 @@ describe('Releases API', () => {
      };
 
      request(options, (err, response, body) => {
-       expect(err.stack).toBeNull();
+       expect(err).toBeNull();
        expect(response.statusCode).toBe(200);
      });
    });
@@ -24,16 +24,20 @@ describe('Releases API', () => {
 
 
 describe('Releases Service', () => {
-  it('should retrieve release info for a given release id', () => {
-    // use mock response
-    ReleasesService.makeRequest = async function(){
-      return JSON.parse(releasesResponse.data);
-    };
 
+  // use mock response
+  beforeEach(function(){
+    ReleasesService.makeRequest = async function(){
+      return JSON.parse(releasesResponse['Item']['data']);
+    };
+  });
+
+  it('should retrieve release info for a given release id', () => {
     ReleasesService.getRelease(testReleaseId)
     .then(info => {
       expect(info.id).toBe('3');
     })
-    .catch(err => expect(err.stack).toBeNull());
+    .catch(err => expect(err).toBeNull());
   });
-})
+
+});
